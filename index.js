00251ca21a06a36e556784d0161707567718c591
Meet dudhat <146607536+meetdudhat13@users.noreply.github.com>
2023-12-16 16:45:57 +0530

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+// var x = document.getElementById('username').value;
+// var y = document.getElementById('password').value;
+
+function submit(){
+   
+    localStorage.setItem('username', username.value);
+    localStorage.setItem('password', password.value);
+}
+function reset(){
+    var a = document.getElementById("username");
+    var b = document.getElementById("password");
+
+}
